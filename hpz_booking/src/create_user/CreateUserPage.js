6edc64d3ddbac618d24688b5f701b5879ce99921
BuildTools <unconfigured@null.spigotmc.org>
2017-07-04 16:46:40 +0800

    Update

--- hpz_booking/src/create_user/CreateUserPage.js
+++ hpz_booking/src/create_user/CreateUserPage.js
@@ -1,12 +1,33 @@
-import React, {Component} from 'react';
+import React, {
+	Component
+} from 'react';
 import ReactDOM from 'react-dom';
+import {
+	Bar
+} from 'react-chartjs-2'
 
 export default class CreateUserPage extends Component {
 
-  render() {
-    return <button>TESTING</button>
-  }
-
-
+	render() {
+    const data = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+        {
+          label: 'My First dataset',
+          backgroundColor: 'rgba(255,99,132,0.2)',
+          borderColor: 'rgba(255,99,132,1)',
+          borderWidth: 1,
+          hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+          hoverBorderColor: 'rgba(255,99,132,1)',
+          data: [65, 59, 80, 81, 56, 55, 40]
+        }
+      ]
+    };
+		return (
+			<div><Bar data={data} options={{
+				maintainAspectRatio: false
+			}}/></div>
+		)
+	}
 
 }
