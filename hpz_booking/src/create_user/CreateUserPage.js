81dd4eee0369a83cd60e555dadca195d8813b9e5
BuildTools <unconfigured@null.spigotmc.org>
2017-07-04 18:35:58 +0800

    Update

--- hpz_booking/src/create_user/CreateUserPage.js
+++ hpz_booking/src/create_user/CreateUserPage.js
@@ -3,14 +3,14 @@ import React, {
 } from 'react';
 import ReactDOM from 'react-dom';
 import {
-	Bar
+	Bar, Bubble
 } from 'react-chartjs-2'
 
 export default class CreateUserPage extends Component {
 
 	render() {
     const data = {
-      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      labels: ['1', 'February', 'March', 'April', 'May', 'June', 'July'],
       datasets: [
         {
           label: 'My First dataset',
@@ -23,10 +23,39 @@ export default class CreateUserPage extends Component {
         }
       ]
     };
+
+		const data2 = {
+  labels: ['January'],
+  datasets: [
+    {
+      label: 'My First dataset',
+      fill: false,
+      lineTension: 0.1,
+      backgroundColor: 'rgba(75,192,192,0.4)',
+      borderColor: 'rgba(75,192,192,1)',
+      borderCapStyle: 'butt',
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: 'rgba(75,192,192,1)',
+      pointBackgroundColor: '#fff',
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      data: [{x:10,y:20,r:5}]
+    }
+  ]
+};
+
 		return (
 			<div><Bar data={data} options={{
 				maintainAspectRatio: false
-			}}/></div>
+			}}/>
+			<Bubble data={data2} /></div>
 		)
 	}
 
